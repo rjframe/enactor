52f2604291d8fdd262879716d4445e09dad00aa3
Ryan Frame <code@ryanjframe.com>
2020-01-19 19:33:23 -0500

    Add my trace function.

---
+++ source/enactor/trace.d
@@ -0,0 +1,10 @@
+module enactor.trace;
+
+void trace(T...)(T args, string func = __FUNCTION__) {
+    import std.stdio : writeln;
+    if (args.length > 0) {
+        debug writeln("*trace: ", func, "- ", args);
+    } else {
+        debug writeln("*trace: ", func);
+    }
+}
